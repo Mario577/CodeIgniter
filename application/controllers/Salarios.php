06b08a7065e65024fd299ec55dd7124ce4555583
Mario577 <107072047+Mario577@users.noreply.github.com>
2022-06-09 16:51:48 -0600

    main

--- application/controllers/Salarios.php
+++ application/controllers/Salarios.php
@@ -3,21 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Salarios extends CI_Controller {
 
-    public function salario(){
+    public function pago(){
 
-        $name= "Marcos Reyes";
-        $name1= "Elena Vasquez";
-        $name2= "Douglas Rodriguez";
-        $name3= "Fabricio Crespin";
+ 
+        $empleados = array("Marcos"=>1200,"Elena"=>1200, "Douglas"=>600);
+        $data = array('empleados'=> $empleados); 
 
-        $salario = "1200";
-        $salario1 = "800";
-        $salario2 = "600";
-        $salario3 = "400";
-
-    
-        $data = array('name'=>$name,'salario'=>$salario,'name1'=>$name1, 'salario1'=>$salario1, 'name2'=>$name2, 'salario2'=>$salario2, 'name3'=>$name3, 'salario3'=>$salario3);
-    
         $this->load->view('vistasalarios',$data);
 
 
--- application/views/vistasalarios.php
+++ application/views/vistasalarios.php
@@ -1,6 +1,9 @@
 <h1>Los salarios de los siguientes<br>
     empleados de la Caja de Credito son: </h1><br>
-    <?php echo $name ?><?php echo $salario ?><br>
-    <?php echo $name1 ?><?php echo $salario1 ?><br>
-    <?php echo $name2 ?><?php echo $salario2 ?><br>
-    <?php echo $name3 ?><?php echo $salario3 ?>
+<?php 
+    foreach ($empleados as $planilla => $salario) {
+        echo " $planilla . $salario <br>";
+    }
+    
+
+?>
