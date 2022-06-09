cb82b1aa7aad7b1177e8e85cd484fff15917a44d
Mario577 <107072047+Mario577@users.noreply.github.com>
2022-06-09 17:04:19 -0600

    segundo commit

--- application/controllers/Salarios.php
+++ application/controllers/Salarios.php
@@ -5,23 +5,10 @@ class Salarios extends CI_Controller {
 
     public function pago(){
 
-<<<<<<< HEAD
  
         $empleados = array("Marcos"=>1200,"Elena"=>1200, "Douglas"=>600);
         $data = array('empleados'=> $empleados); 
 
-=======
-        $planilla= "Marcos Reyes" . "Elena Vasquez" . "Douglas Rodriguez" . "Fabricio Crespin" ;
-        $salario = "1200" . "800" . "600" . "400";
-    
-        $data = array('name'=>$planilla,'salario'=>$salario,);
-
-        foreach ($data as $planilla => $salario) {
-
-            echo " $planilla $salario <br>";
-        }
-    
->>>>>>> 3e6380c15e93146e4264a5d673b274bfb66d35d2
         $this->load->view('vistasalarios',$data);
 
 
