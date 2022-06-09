3e6380c15e93146e4264a5d673b274bfb66d35d2
Mario577 <107072047+Mario577@users.noreply.github.com>
2022-06-09 12:33:04 -0600

    main

--- application/controllers/Salarios.php
+++ application/controllers/Salarios.php
@@ -5,18 +5,15 @@ class Salarios extends CI_Controller {
 
     public function salario(){
 
-        $name= "Marcos Reyes";
-        $name1= "Elena Vasquez";
-        $name2= "Douglas Rodriguez";
-        $name3= "Fabricio Crespin";
-
-        $salario = "1200";
-        $salario1 = "800";
-        $salario2 = "600";
-        $salario3 = "400";
-
+        $planilla= "Marcos Reyes" . "Elena Vasquez" . "Douglas Rodriguez" . "Fabricio Crespin" ;
+        $salario = "1200" . "800" . "600" . "400";
     
-        $data = array('name'=>$name,'salario'=>$salario,'name1'=>$name1, 'salario1'=>$salario1, 'name2'=>$name2, 'salario2'=>$salario2, 'name3'=>$name3, 'salario3'=>$salario3);
+        $data = array('name'=>$planilla,'salario'=>$salario,);
+
+        foreach ($data as $planilla => $salario) {
+
+            echo " $planilla $salario <br>";
+        }
     
         $this->load->view('vistasalarios',$data);
 
--- application/views/vistasalarios.php
+++ application/views/vistasalarios.php
@@ -1,6 +1,4 @@
 <h1>Los salarios de los siguientes<br>
     empleados de la Caja de Credito son: </h1><br>
     <?php echo $name ?><?php echo $salario ?><br>
-    <?php echo $name1 ?><?php echo $salario1 ?><br>
-    <?php echo $name2 ?><?php echo $salario2 ?><br>
-    <?php echo $name3 ?><?php echo $salario3 ?>
+    
