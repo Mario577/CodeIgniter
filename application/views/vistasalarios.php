5d7387384403fd893559fb942a84a5cc90b1e05a
Mario577 <107072047+Mario577@users.noreply.github.com>
2022-06-10 09:11:51 -0600

    cambio anterior

--- application/views/vistasalarios.php
+++ application/views/vistasalarios.php
@@ -1,6 +1,5 @@
 <h1>Los salarios de los siguientes<br>
     empleados de la Caja de Credito son: </h1><br>
-<<<<<<< HEAD
 <?php 
     foreach ($empleados as $planilla => $salario) {
         echo " $planilla . $salario <br>";
@@ -8,7 +7,3 @@
     
 
 ?>
-=======
-    <?php echo $name ?><?php echo $salario ?><br>
-    
->>>>>>> 3e6380c15e93146e4264a5d673b274bfb66d35d2
