d03aa4acebb194b3b983c43f0b34a2b30fb0d905
Mario577 <107072047+Mario577@users.noreply.github.com>
2022-06-10 10:39:46 -0600

    cambio datos

--- application/views/usuarios.php
+++ application/views/usuarios.php
@@ -1,8 +1,8 @@
 <h1> Tabla de Datos </h1>
 <form name="formulario" action="Listas.php" method="get">
-dui<input type="text" name= "dui" id="dui" /><br>
-Nombre <input type="text" name="nombre" id="nombre" /><br>
-Contraseña<input type="password" name="contra" id="contra"/>
+dui<input type="text" name= "dui" id="dui_1" /><br>
+Nombre <input type="text" name="nombre" id="nombre_1" /><br>
+Contraseña<input type="password" name="contra" id="contra_1"/>
 <button type="submit">Enviar</button> 
 </form>
 <?php 
