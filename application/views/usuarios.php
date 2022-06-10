d90a6b6156e61cc7c013d8a954694a582f316740
Mario577 <107072047+Mario577@users.noreply.github.com>
2022-06-10 09:38:53 -0600

    formulario

--- application/views/usuarios.php
+++ application/views/usuarios.php
@@ -1,10 +1,13 @@
 <h1> Tabla de Datos </h1>
+<form action="" method="post">
+dui<input type="text" id="nombre" /><br>
+Nombre <input type="text" id="nombre" /><br>
+Contraseña<input type="password" id="contra"/>
+<button type="submit">Enviar</button> 
+</form>
 <?php 
-
 foreach ($usuario as $datos => $nombre) {
      
     echo " $datos . $nombre <br>";
 }
-
-
 ?>
