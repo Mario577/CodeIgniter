ccc0b938ce909c8edfbc0bde9e09e064781ff448
Mario577 <107072047+Mario577@users.noreply.github.com>
2022-06-10 09:16:27 -0600

    cambio a string

--- application/controllers/Listas.php
+++ application/controllers/Listas.php
@@ -5,7 +5,7 @@ class Listas extends CI_Controller {
 
     public function Lista(){
 
-        $usuario = array('dui' =>06369537-7 ,'name'=>"mario", 'edad'=>20);
+        $usuario = array('dui' =>"06369537-7" ,'name'=>'mario', 'edad'=>"20");
         $data = array('usuario' => $usuario,);
 
         $this->load->view('usuarios',$data);
