552789c290577f701f8c06bf0d77b347a398c88f
Mario577 <107072047+Mario577@users.noreply.github.com>
2022-06-10 09:12:59 -0600

    cambio proyecto

--- application/controllers/Listas.php
+++ application/controllers/Listas.php
@@ -4,6 +4,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Listas extends CI_Controller {
 
     public function Lista(){
+
+        $usuario = array('dui' =>06369537-7 ,'name'=>"mario", 'edad'=>20);
+        $data = array('usuario' => $usuario,);
+
+        $this->load->view('usuarios',$data);
+
         
     }
 
---
+++ application/views/usuarios.php
@@ -0,0 +1,10 @@
+<h1> Tabla de Datos </h1>
+<?php 
+
+foreach ($usuario as $datos => $nombre) {
+     
+    echo " $datos . $nombre <br>";
+}
+
+
+?>
