250bc5f13d4df1dd649cb6a2424566e88fff97e7
Mario577 <107072047+Mario577@users.noreply.github.com>
2022-06-10 11:18:19 -0600

    main

--- application/views/usuarios.php
+++ application/views/usuarios.php
@@ -1,6 +1,6 @@
 <h1> Tabla de Datos </h1>
 <form name="formulario" action="Listas.php" method="get">
-dui<input type="text" name= "dui" id="dui_1" /><br>
+Dui<input type="text" name= "dui" id="dui_1" /><br>
 Nombre <input type="text" name="nombre" id="nombre_1" /><br>
 Contraseña<input type="password" name="contra" id="contra_1"/>
 <button type="submit">Enviar</button> 
