4235487fdf70dfed680e4ad4d9ede1b58aa2a0be
Mario577 <107072047+Mario577@users.noreply.github.com>
2022-06-11 08:16:40 -0600

    cambio

--- application/views/usuarios.php
+++ application/views/usuarios.php
@@ -1,21 +1,10 @@
-<h1> Tabla de Datos </h1>
+<h1>Tabla de Datos </h1>
 <form name="formulario" action="Listas.php" method="get">
-Dui<input type="text" name= "dui" id="dui_1" /><br>
-<button type="submit">Enviar</button> 
-</form>
-
-
-
-
-
-<?php 
+    Dui<input type="text" name="dui" id="dui_1" /><br>
+    <button type="submit">Enviar</button></form>
     
-
-    
-        foreach ($empleados as $row) {
-     
-            echo " $row->dui <br>";
-            echo " $row->nombre <br>";
-            echo " $row->edad <br>";
-            
+    <?php foreach ($empleados as $row){ 
+        echo " $row->dui <br>";
+        echo " $row->nombre <br>";
+        echo " $row->edad <br>";
         }
