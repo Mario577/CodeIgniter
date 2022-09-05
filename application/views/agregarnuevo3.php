7f3fead6a8f28f0a50fb37d8f9fd785f4e298e7b
Mario577 <107072047+Mario577@users.noreply.github.com>
2022-09-05 10:21:48 -0600

    Eliminacion de footer

--- application/views/agregarnuevo3.php
+++ application/views/agregarnuevo3.php
@@ -222,9 +222,7 @@
 
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
-    <footer class="foot">
-        <h3> Creado por Mario Acevedo</h3>
-    </footer>
+    
 </body>
 
 </html>
